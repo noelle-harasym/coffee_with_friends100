f1a08f1a90f72674ecda1d9c09ff7f114d8a47c7
anonymous anonymous <noelle.harasym@gmail.com>
2022-03-13 19:35:48 +0000

    Generate graphiti resources for coffee_orders

---
+++ app/resources/coffee_order_resource.rb
@@ -0,0 +1,19 @@
+class CoffeeOrderResource < ApplicationResource
+  attribute :id, :integer, writable: false
+  attribute :created_at, :datetime, writable: false
+  attribute :updated_at, :datetime, writable: false
+  attribute :user_id, :integer
+  attribute :coffee_type_id, :integer
+  attribute :cafe, :string
+  attribute :dairy_type_id, :integer
+  attribute :modifications_other, :string
+  attribute :iced, :boolean
+  attribute :picture, :string
+  attribute :date, :date
+  attribute :rating, :integer
+
+  # Direct associations
+
+  # Indirect associations
+
+end
